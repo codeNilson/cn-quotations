8eb016666d9fb399e01e9630a5592c585a182432
fcodenilson <fcodenilson@gmail.com>
2025-07-17 19:56:36 +0000

    feature: sidebar toggle

--- src/App.tsx
+++ src/App.tsx
@@ -3,67 +3,73 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Card from "./components/Card.tsx";
 import Table from "./components/Table.tsx";
 import Sidebar from "./components/Sidebar.tsx"
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
 
   return (
     <div className="bg-gray-100 min-h-screen gap-3 flex">
       <Sidebar />
-      <main className="w-full">
-        <div className="cards-container">
-          <h2 className="text-2xl font-bold tracking-tight">Dashboard</h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 px-5 mt-5">
-            <Card>
-              <div className="flex justify-between items-center">
-                <span>Concluídas</span>
-                <FontAwesomeIcon icon={faCircleCheck} className="text-green-600" />
-              </div>
-              <div>
-                <p className="text-3xl font-bold">64</p>
-              </div>
-            </Card>
-            <Card>
-              <div className="flex justify-between items-center">
-                <span>Aguardando Resposta</span>
-                <FontAwesomeIcon icon={faClock} className="text-blue-600" />
-              </div>
-              <div>
-                <p className="text-3xl font-bold">3</p>
-              </div>
-            </Card>
-            <Card>
-              <div className="flex justify-between items-center">
-                <span>Sem Retorno</span>
-                <FontAwesomeIcon icon={faCircleXmark} className="text-red-600" />
-              </div>
-              <div>
-                <p className="text-3xl font-bold">3</p>
-              </div>
-            </Card>
-            <Card>
-              <div className="flex justify-between items-center">
-                <span>Total de cotações (Mês)</span>
-                <FontAwesomeIcon icon={faScrewdriverWrench} className="text-red-800" />
-              </div>
-              <div>
-                <p className="text-3xl font-bold">12</p>
-              </div>
-            </Card>
+      <div>
+        <button className="btn md:hidden">
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+        <main className="w-full">
+          <div className="cards-container">
+            <h2 className="text-2xl font-bold tracking-tight">Dashboard</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 px-5 mt-5">
+              <Card>
+                <div className="flex justify-between items-center">
+                  <span>Concluídas</span>
+                  <FontAwesomeIcon icon={faCircleCheck} className="text-green-600" />
+                </div>
+                <div>
+                  <p className="text-3xl font-bold">64</p>
+                </div>
+              </Card>
+              <Card>
+                <div className="flex justify-between items-center">
+                  <span>Aguardando Resposta</span>
+                  <FontAwesomeIcon icon={faClock} className="text-blue-600" />
+                </div>
+                <div>
+                  <p className="text-3xl font-bold">3</p>
+                </div>
+              </Card>
+              <Card>
+                <div className="flex justify-between items-center">
+                  <span>Sem Retorno</span>
+                  <FontAwesomeIcon icon={faCircleXmark} className="text-red-600" />
+                </div>
+                <div>
+                  <p className="text-3xl font-bold">3</p>
+                </div>
+              </Card>
+              <Card>
+                <div className="flex justify-between items-center">
+                  <span>Total de cotações (Mês)</span>
+                  <FontAwesomeIcon icon={faScrewdriverWrench} className="text-red-800" />
+                </div>
+                <div>
+                  <p className="text-3xl font-bold">12</p>
+                </div>
+              </Card>
+            </div>
           </div>
-        </div>
-        <div>
-          <div className="flex justify-between items-center p-3 mt-5">
-            <h4 className="font-bold text-2xl tracking-tight">Cotações recentes</h4>
-            <button className="btn btn-primary text-white flex items-center gap-3">
-              <FontAwesomeIcon icon={faCirclePlus} className="text-2xl md:text-md" />
-              <p className="hidden md:block whitespace-nowrap">Nova Cotação</p>
-            </button>
+          <div>
+            <div className="flex justify-between items-center p-3 mt-5">
+              <h4 className="font-bold text-2xl tracking-tight">Cotações recentes</h4>
+              <button className="btn btn-primary text-white flex items-center gap-3">
+                <FontAwesomeIcon icon={faCirclePlus} className="text-2xl md:text-md" />
+                <p className="hidden md:block whitespace-nowrap">Nova Cotação</p>
+              </button>
+            </div>
+            <div className="overflow-x-auto shadow-md">
+              <Table />
+            </div>
           </div>
-          <div className="overflow-x-auto shadow-md">
-            <Table />
-          </div>
-        </div>
-      </main>
+        </main>
+      </div>
     </div>
   )
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,35 +1,45 @@
+import { useState } from "react";
+
+
 export default function Sidebar() {
+
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
     return (
-        <aside className="w-64 bg-gray-50">
-            <div className="border-b-2 p-3 border-gray-100">
-                <h1>Central Nordeste</h1>
-            </div>
-            <div>
-                <nav>
-                    <ul className="flex flex-col gap-3 p-3">
-                        <li className="text-sm text-gray-500">
-                            <button className="btn w-full text-left hover:text-orange-600">
-                                Dashboard
-                            </button>
-                        </li>
-                        <li className="text-sm text-gray-500">
-                            <button className="btn w-full text-left hover:text-orange-600">
-                                Cotações
-                            </button>
-                        </li>
-                        <li className="text-sm text-gray-500">
-                            <button className="btn w-full text-left hover:text-orange-600">
-                                Peças
-                            </button>
-                        </li>
-                        <li className="text-sm text-gray-500">
-                            <button className="btn w-full text-left hover:text-orange-600">
-                                Máquinas
-                            </button>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </aside>
+        <>
+            <aside className={`w-80 bg-gray-50 z-50 fixed md:static transition duration-300 ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0`}>
+                <div className="border-b-2 p-3 border-gray-100">
+                    <h1>Central Nordeste</h1>
+                </div>
+                <div>
+                    <nav>
+                        <ul className="flex flex-col gap-1 p-3">
+                            <li className="text-sm text-gray-500">
+                                <button className="btn w-full text-left hover:text-orange-600">
+                                    <span>
+                                        Dashboard
+                                    </span>
+                                </button>
+                            </li>
+                            <li className="text-sm text-gray-500">
+                                <button className="btn w-full text-left hover:text-orange-600">
+                                    Cotações
+                                </button>
+                            </li>
+                            <li className="text-sm text-gray-500">
+                                <button className="btn w-full text-left hover:text-orange-600">
+                                    Peças
+                                </button>
+                            </li>
+                            <li className="text-sm text-gray-500">
+                                <button className="btn w-full text-left hover:text-orange-600">
+                                    Máquinas
+                                </button>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </aside>
+        </>
     )
 }
